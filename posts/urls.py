6bfd99675aa1d7a28776f283fce7479f4737de74
unknown <iyadangky@naver.com>
2022-07-02 16:18:23 +0900

    추가기록 기능 탑재

--- posts/urls.py
+++ posts/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('<int:post_id>/edit/', views.edit, name="edit"),
     path('<int:post_id>/update/', views.update, name="update"),
+    path('<int:post_id>/add/', views.add, name="add"),
+    path('<int:post_id>/record/', views.record, name="record"),
     path('download/', views.file_download, name='file_download'),
 ]
